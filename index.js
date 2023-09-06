d5c84a4a1c286bcd5ab9b5feb79f5b2d800b3d1f
Lucasfsa <lucas792azevedo@gmail.com>
2023-09-05 23:21:12 -0300

    configuração de novo model

--- index.js
+++ index.js
@@ -1,47 +1,33 @@
 import { config_openAI, config_telegram } from './src/config.js';
-import { config_mongodb } from './src/database.js'
 
 const openai = config_openAI()
 const telegram_bot = config_telegram()
-const database = config_mongodb()
 
 telegram_bot.on('message', async msg => {
-    let conversationHistory = [];
-    let messages;
 
     const chat_id = msg.chat.id;
-    const messageText = msg.text;
-    const chatHistory = await database.find({chat_id}).toArray();
 
-    if(chatHistory.length){
-        for (const document of chatHistory) {
-             conversationHistory = document.data
-          }
-
-          messages = [ 
-            {"role":"user", "content": messageText}
-        ]
-
-    }else{
-        database.insertMany([{chat_id, data: conversationHistory}]);
-        messages = [ 
-            {"role": "system", "content": "You are a salesperson at Jailson Móveis, presents itself as a virtual attendantYour, your job is to talk about your stock of products, commercial and sales matters of the company, if it is a question outside this context, inform that it is not possible to answer"},
-            {"role":"user", "content": messageText}
-        ]
-    }
-
+    const messages = [
+        {
+            role: 'system',
+            content:"Jailsons bot is a virtual assistant, it is responsible for informing about products, prices, stock, and general information about the company Jailson Móveis. It can also provide information about the company's history, mission, values, address, and working hours."
+        },
+        {
+            role: 'user',
+            content: msg.text
+        }
+      ]
+    
+ 
     openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: "ft:gpt-3.5-turbo-0613:personal:bot-training:7vH0AHp9",
         messages, 
-        temperature: 1, 
-        max_tokens: 4000 
+        temperature: 0.4, 
+        max_tokens: 200
     })
     .then((res) => {
         const reply = res.choices[0].message.content;
         telegram_bot.sendMessage(chat_id,reply);
-         
-        messages.push({ role: "assistant", content: reply });
-        database.updateOne({chat_id}, { $push: { data: messages} });
     })
     .catch((error) => {
         telegram_bot.sendMessage(chat_id,`❌ OpenAI Response Error: ${error}`);
