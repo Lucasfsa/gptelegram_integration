b87f34e82fb4f53eac3e7708663c3b8f804e3d2c
Lucasfsa <lucas792azevedo@gmail.com>
2023-09-15 23:13:08 -0300

    implementação de registro de logs

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import { config_openAI, config_telegram } from './src/config.js';
 const openai = config_openAI()
 const telegram_bot = config_telegram()
 
+console.log('Aplicação rodando 🟢')
+
 telegram_bot.on('message', async msg => {
+
+    console.log(Date() + ' ---- Nova mensagem do telegram detectada 📝🟢')
 
     const chat_id = msg.chat.id;
 
@@ -17,21 +21,26 @@ telegram_bot.on('message', async msg => {
             content: msg.text
         }
       ]
-    
- 
+
     openai.chat.completions.create({
         model: "ft:gpt-3.5-turbo-0613:personal:bot-training:7vH0AHp9",
         messages, 
-        temperature: 0.4, 
+        temperature: 0.2, 
         max_tokens: 200
     })
     .then((res) => {
         const reply = res.choices[0].message.content;
         telegram_bot.sendMessage(chat_id,reply);
+
+        console.log(Date() + ' ---- Mensagem de resposta da IA enviada 📬🟢')
+
     })
     .catch((error) => {
         telegram_bot.sendMessage(chat_id,`❌ OpenAI Response Error: ${error}`);
         console.log(error)
+
+        console.log(Date() +' ---- Erro na resposta da IA 📬❌')
+
     });    
     
 });
